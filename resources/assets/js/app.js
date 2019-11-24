36c4f81bc8c6f205df906f39a09218e47cfcda20
AhmedMousa2020 <maxsteel2020@gmail.com>
2019-11-24 02:47:44 +0200

    Project Structure

---
+++ resources/assets/js/app.js
@@ -0,0 +1,98 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+import { Form, HasError, AlertError } from 'vform'
+import moment from 'moment';
+
+import swal from 'sweetalert2'
+window.swal = swal;
+
+const toast = swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000
+});
+
+window.toast = toast;
+
+window.Form=Form;
+Vue.component(HasError.name, HasError)
+Vue.component(AlertError.name, AlertError)
+
+import Dashboard from './components/Dashboard'
+import Profile from './components/Profile'
+import Users from './components/Users'
+import Developer from './components/Developer'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueProgressBar from 'vue-progressbar'
+Vue.use(VueProgressBar, {
+    color: 'rgb(143, 255, 199)',
+    failedColor: 'red',
+    height: '3px'
+  })
+
+export const  routes = [
+    { path: '/dashboard', component: require('./components/Dashboard.vue') },
+    { path: '/developer', component: require('./components/Developer.vue') },
+    { path: '/users', component: require('./components/Users.vue') },
+    { path: '/Profile', component: require('./components/Profile.vue') },
+    { path: '*', component: require('./components/NotFound.vue') }
+  ]
+
+const router = new VueRouter({
+    mode:'history',
+    routes:[
+        {path: '/Profile', component: Profile},
+        {path: '/Dashboard', component: Dashboard},
+        {path: '/developer', component: Developer},
+        {path: '/Users', component: Users}] // short for `routes: routes`
+  })
+
+Vue.filter('upText', function(text){
+    return text.charAt(0).toUpperCase() + text.slice(1)
+});
+
+Vue.filter('myDate',function(created){
+    return moment(created).format('MMMM Do YYYY');
+});
+
+let Fire = new Vue();
+window.Fire = Fire;
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
+
+
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+var app = new Vue({
+    el:'#app',
+    router
+});
